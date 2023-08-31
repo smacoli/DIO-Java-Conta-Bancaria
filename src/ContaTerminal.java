e37f1c4a1a3ddfed5628cfba6c6657ec7033ad07
Suzana Machado <suzanapaulaoliveira@gmail.com>
2023-08-31 20:49:36 -0300

    Bootcamp Santander

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -7,6 +7,9 @@ public class ContaTerminal {
         String agencia;
         String nomeCliente;
         float saldo;
+        float saque;
+        String realizar;
+
 
         Scanner scanner = new Scanner(System.in);
 
@@ -22,7 +25,25 @@ public class ContaTerminal {
 
         System.out.print("Insira seu saldo: ");
         saldo = scanner.nextFloat();
+        scanner.nextLine();
 
-        System.out.println("Ola " + nomeCliente + ", obrigado por criar uma conta em nosso banco. O numero de sua agência é " + agencia + ", conta número " + numeroConta + " e seu saldo de " + saldo + " já está disponível para saque.");
+        System.out.println("Ola " + nomeCliente + ", obrigada por criar uma conta em nosso banco. \nO numero de sua agência é " + agencia + ", conta número " + numeroConta + " e seu saldo de " + saldo + " já está disponível para saque.");
+
+        System.out.println("Gostaria de realizar um saque? S/N");
+        realizar = scanner.nextLine();
+
+        if(realizar.equalsIgnoreCase("S")){
+            System.out.print("::::::::::::::Sacar dinheiro::::::::::::::\nInsira o valor do saque: ");
+            saque = scanner.nextFloat();
+
+            if(saque < saldo){
+                saldo = saldo - saque;
+            }
+
+            System.out.println("Novo saldo = " + saldo);
+        } else {
+            System.out.println("Obrigada por acessar nosso caixa online.");
+        }
+
     }
 }
